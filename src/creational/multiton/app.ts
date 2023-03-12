acb782f335caa2ef0e9fc3d4e4d96808d7246db3
Yobuligo <hoffmannpeter82@gmail.com>
2023-03-12 23:10:44 +0100

    Implement Multiton

---
+++ src/creational/multiton/app.ts
@@ -0,0 +1,17 @@
+class Multiton {
+  private static instances: Map<string, Multiton> = new Map();
+
+  private constructor(public key: string) {}
+
+  public static getInstance(key: string): Multiton {
+    let instance = this.instances.get(key);
+    if (!instance) {
+      instance = new Multiton(key);
+      this.instances.set(key, instance);
+    }
+    return instance;
+  }
+}
+
+const multitonFirst = Multiton.getInstance("first");
+const multitonSecond = Multiton.getInstance("second");
