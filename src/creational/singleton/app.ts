4f737741f99e01c3e43857caf9e0cc43d5519bfc
Yobuligo <hoffmannpeter82@gmail.com>
2023-03-12 23:04:55 +0100

    Implement Singleton

---
+++ src/creational/singleton/app.ts
@@ -0,0 +1,14 @@
+class Singleton {
+  private static instance: Singleton;
+
+  private constructor() {}
+
+  public static getInstance(): Singleton {
+    if (!this.instance) {
+      this.instance = new Singleton();
+    }
+    return this.instance;
+  }
+}
+
+const singleton = Singleton.getInstance();
