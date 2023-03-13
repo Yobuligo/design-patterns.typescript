21f1817ffe1c4798971ffc3359cbb841bd925aa2
Yobuligo <hoffmannpeter82@gmail.com>
2023-03-13 10:12:16 +0100

    Implement Chain of Responsibility

--- dist/behavioral/chainOfResponsibility/app.js
+++ dist/behavioral/chainOfResponsibility/app.js
@@ -1,2 +1,55 @@
 "use strict";
+var ChainOfResponsibility;
+(function (ChainOfResponsibility) {
+    var Request = /** @class */ (function () {
+        function Request(operation, value1, value2) {
+            this.operation = operation;
+            this.value1 = value1;
+            this.value2 = value2;
+        }
+        return Request;
+    }());
+    var Addition = /** @class */ (function () {
+        function Addition(follower) {
+            this.follower = follower;
+        }
+        Addition.prototype.handle = function (request) {
+            if (request.operation === "+") {
+                return request.value1 + request.value2;
+            }
+            if (this.follower) {
+                return this.follower.handle(request);
+            }
+        };
+        return Addition;
+    }());
+    var Subtraction = /** @class */ (function () {
+        function Subtraction(follower) {
+            this.follower = follower;
+        }
+        Subtraction.prototype.handle = function (request) {
+            if (request.operation === "-") {
+                return request.value1 - request.value2;
+            }
+            if (this.follower) {
+                return this.follower.handle(request);
+            }
+        };
+        return Subtraction;
+    }());
+    var Handler = /** @class */ (function () {
+        function Handler() {
+            this.follower = new Addition(new Subtraction(undefined));
+        }
+        Handler.prototype.handle = function (request) {
+            var _a;
+            return (_a = this.follower) === null || _a === void 0 ? void 0 : _a.handle(request);
+        };
+        return Handler;
+    }());
+    var handler = new Handler();
+    console.log(handler.handle(new Request("+", 7, 3)));
+    console.log(handler.handle(new Request("-", 7, 3)));
+    console.log(handler.handle(new Request("*", 7, 3)));
+})(ChainOfResponsibility || (ChainOfResponsibility = {}));
 //# sourceMappingURL=app.js.map
---
+++ src/behavioral/chainOfResponsibility/app.ts
@@ -0,0 +1,60 @@
+namespace ChainOfResponsibility {
+  interface IRequest {
+    readonly operation: string;
+    readonly value1: number;
+    readonly value2: number;
+  }
+
+  interface IHandler {
+    follower: IHandler | undefined;
+    handle(request: IRequest): number | undefined;
+  }
+
+  class Request implements IRequest {
+    constructor(
+      readonly operation: string,
+      readonly value1: number,
+      readonly value2: number
+    ) {}
+  }
+
+  class Addition implements IHandler {
+    constructor(public follower: IHandler | undefined) {}
+
+    handle(request: IRequest): number | undefined {
+      if (request.operation === "+") {
+        return request.value1 + request.value2;
+      }
+
+      if (this.follower) {
+        return this.follower.handle(request);
+      }
+    }
+  }
+
+  class Subtraction implements IHandler {
+    constructor(public follower: IHandler | undefined) {}
+    handle(request: IRequest): number | undefined {
+      if (request.operation === "-") {
+        return request.value1 - request.value2;
+      }
+
+      if (this.follower) {
+        return this.follower.handle(request);
+      }
+    }
+  }
+
+  class Handler implements IHandler {
+    follower: IHandler = new Addition(new Subtraction(undefined));
+
+    handle(request: IRequest): number | undefined {
+      return this.follower?.handle(request);
+    }
+  }
+
+  const handler = new Handler();
+  console.log(handler.handle(new Request("+", 7, 3)));
+  console.log(handler.handle(new Request("-", 7, 3)));
+  console.log(handler.handle(new Request("*", 7, 3)));
+}
