bd669639b4b6be755abbffa230ee2e5cd9f4861e
Yobuligo <hoffmannpeter82@gmail.com>
2023-03-14 22:04:52 +0100

    Extend example for prototype

--- dist/creational/prototype/app.js
+++ dist/creational/prototype/app.js
@@ -2,21 +2,24 @@
 var Prototype;
 (function (Prototype) {
     var Cell = /** @class */ (function () {
-        function Cell(cloneCycles) {
+        function Cell(dna, cloneCycles) {
+            this.dna = dna;
             this.cloneCycles = cloneCycles;
-            this._name = "Alfred";
         }
         Cell.prototype.clone = function () {
             if (this.cloneCycles === 0) {
-                throw new Error("Cell is not cloneable anymore.");
+                throw new Error("Cell with cell code '".concat(this.cellCode, "' is not cloneable anymore."));
             }
-            var cell = new Cell(this.cloneCycles - 1);
-            cell._name = this._name;
+            var cell = new Cell(this.dna, this.cloneCycles - 1);
+            cell._cellCode = this.cellCode;
             return cell;
         };
-        Object.defineProperty(Cell.prototype, "name", {
+        Object.defineProperty(Cell.prototype, "cellCode", {
             get: function () {
-                return this._name;
+                if (!this._cellCode) {
+                    this._cellCode = new Date().getTime() * Math.random() * 10000;
+                }
+                return this._cellCode;
             },
             enumerable: false,
             configurable: true
@@ -25,11 +28,11 @@ var Prototype;
     }());
     var cloneCell = function (cell) {
         cell = cell.clone();
-        console.log("I am the cell '".concat(cell.name, "' and can be cloned '").concat(cell.cloneCycles, "' times more."));
+        console.log("I am cell with cell code '".concat(cell.cellCode, "', dna '").concat(cell.dna, "' and can be cloned '").concat(cell.cloneCycles, "' times."));
         return cell;
     };
     try {
-        var cell = new Cell(3);
+        var cell = new Cell("6712376", 3);
         cell = cloneCell(cell);
         cell = cloneCell(cell);
         cell = cloneCell(cell);
@@ -38,5 +41,7 @@ var Prototype;
     catch (error) {
         console.log(error.message);
     }
+    var cancer = new Cell("6712376", 3);
+    cloneCell(cancer);
 })(Prototype || (Prototype = {}));
 //# sourceMappingURL=app.js.map
--- src/creational/prototype/app.ts
+++ src/creational/prototype/app.ts
@@ -1,33 +1,36 @@
 namespace Prototype {
   class Cell {
-    private _name: string = "Alfred";
+    private _cellCode: number | undefined;
 
-    constructor(readonly cloneCycles: number) {}
+    constructor(readonly dna: string, readonly cloneCycles: number) {}
 
     clone(): Cell {
       if (this.cloneCycles === 0) {
-        throw new Error("Cell is not cloneable anymore.");
+        throw new Error(`Cell with cell code '${this.cellCode}' is not cloneable anymore.`);
       }
-      const cell = new Cell(this.cloneCycles - 1);
-      cell._name = this._name;
+      const cell = new Cell(this.dna, this.cloneCycles - 1);
+      cell._cellCode = this.cellCode;
       return cell;
     }
 
-    get name(): string {
-      return this._name;
+    get cellCode(): number {
+      if (!this._cellCode) {
+        this._cellCode = new Date().getTime() * Math.random() * 10000;
+      }
+      return this._cellCode;
     }
   }
 
   const cloneCell = (cell: Cell): Cell => {
     cell = cell.clone();
     console.log(
-      `I am the cell '${cell.name}' and can be cloned '${cell.cloneCycles}' times more.`
+      `I am cell with cell code '${cell.cellCode}', dna '${cell.dna}' and can be cloned '${cell.cloneCycles}' times.`
     );
     return cell;
   };
 
   try {
-    let cell = new Cell(3);
+    let cell = new Cell("6712376", 3);
     cell = cloneCell(cell);
     cell = cloneCell(cell);
     cell = cloneCell(cell);
@@ -35,4 +38,7 @@ namespace Prototype {
   } catch (error) {
     console.log((error as Error).message);
   }
+
+  const cancer = new Cell("6712376", 3);
+  cloneCell(cancer);
 }
