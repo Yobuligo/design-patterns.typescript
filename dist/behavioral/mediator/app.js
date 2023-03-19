e187df9dbe31442781fe138c71f89836343b8d42
Yobuligo <hoffmannpeter82@gmail.com>
2023-03-19 18:40:21 +0100

    Implement mediator

--- dist/behavioral/mediator/app.js
+++ dist/behavioral/mediator/app.js
@@ -1,2 +1,67 @@
 "use strict";
+var Mediator;
+(function (Mediator) {
+    var Button = /** @class */ (function () {
+        function Button() {
+            this._enabled = true;
+        }
+        Object.defineProperty(Button.prototype, "enabled", {
+            get: function () {
+                return this._enabled;
+            },
+            set: function (value) {
+                this._enabled = value;
+                console.log("Button was '".concat(this._enabled ? "enabled" : "disabled", "'"));
+            },
+            enumerable: false,
+            configurable: true
+        });
+        return Button;
+    }());
+    var Input = /** @class */ (function () {
+        function Input() {
+            this.onChangeHandler = [];
+            this._value = "";
+        }
+        Object.defineProperty(Input.prototype, "value", {
+            set: function (value) {
+                var _this = this;
+                if (this._value === value) {
+                    return;
+                }
+                this._value = value;
+                this.onChangeHandler.forEach(function (handler) { return handler(_this._value); });
+            },
+            enumerable: false,
+            configurable: true
+        });
+        Input.prototype.onChange = function (handler) {
+            this.onChangeHandler.push(handler);
+        };
+        return Input;
+    }());
+    /**
+     * Dialog is the mediator that takes over the controlling of the components (here button and input)
+     * without the need that these components need each other.
+     * It contains the business logic.
+     */
+    var Dialog = /** @class */ (function () {
+        function Dialog() {
+            var _this = this;
+            this.button = new Button();
+            this.input = new Input();
+            this.onInputValueChangeHandler = function (value) {
+                _this.button.enabled = value.includes("@");
+            };
+            this.input.onChange(this.onInputValueChangeHandler);
+        }
+        Dialog.prototype.enterInputValue = function (value) {
+            this.input.value = value;
+        };
+        return Dialog;
+    }());
+    var dialog = new Dialog();
+    dialog.enterInputValue("test");
+    dialog.enterInputValue("mediator@test.com");
+})(Mediator || (Mediator = {}));
 //# sourceMappingURL=app.js.map
--- src/behavioral/mediator/app.ts
+++ src/behavioral/mediator/app.ts
@@ -1,3 +1,58 @@
 namespace Mediator {
-  // Todo
+  class Button {
+    private _enabled: boolean = true;
+
+    get enabled(): boolean {
+      return this._enabled;
+    }
+
+    set enabled(value: boolean) {
+      this._enabled = value;
+      console.log(`Button was '${this._enabled ? "enabled" : "disabled"}'`);
+    }
+  }
+
+  class Input {
+    private onChangeHandler: ((value: string) => void)[] = [];
+    private _value: string = "";
+
+    set value(value: string) {
+      if (this._value === value) {
+        return;
+      }
+
+      this._value = value;
+      this.onChangeHandler.forEach((handler) => handler(this._value));
+    }
+
+    onChange(handler: (value: string) => void): void {
+      this.onChangeHandler.push(handler);
+    }
+  }
+
+  /**
+   * Dialog is the mediator that takes over the controlling of the components (here button and input)
+   * without the need that these components need each other.
+   * It contains the business logic.
+   */
+  class Dialog {
+    private button = new Button();
+    private input = new Input();
+
+    constructor() {
+      this.input.onChange(this.onInputValueChangeHandler);
+    }
+
+    enterInputValue(value: string) {
+      this.input.value = value;
+    }
+
+    private onInputValueChangeHandler = (value: string) => {
+      this.button.enabled = value.includes("@");
+    };
+  }
+
+  const dialog = new Dialog();
+  dialog.enterInputValue("test");
+  dialog.enterInputValue("mediator@test.com");
 }
