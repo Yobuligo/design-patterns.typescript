edb7556bf5253903680c222d220115d32f578aaf
Yobuligo <hoffmannpeter82@gmail.com>
2023-03-19 22:42:35 +0100

    Rename events and event handlers

--- src/behavioral/mediator/app.ts
+++ src/behavioral/mediator/app.ts
@@ -13,7 +13,7 @@ namespace Mediator {
   }
 
   class Input {
-    private onChangeHandler: ((value: string) => void)[] = [];
+    private onChangeHandlers: ((value: string) => void)[] = [];
     private _value: string = "";
 
     set value(value: string) {
@@ -22,11 +22,11 @@ namespace Mediator {
       }
 
       this._value = value;
-      this.onChangeHandler.forEach((handler) => handler(this._value));
+      this.onChangeHandlers.forEach((handler) => handler(this._value));
     }
 
     onChange(handler: (value: string) => void): void {
-      this.onChangeHandler.push(handler);
+      this.onChangeHandlers.push(handler);
     }
   }
 
@@ -40,14 +40,14 @@ namespace Mediator {
     private input = new Input();
 
     constructor() {
-      this.input.onChange(this.onInputValueChangeHandler);
+      this.input.onChange(this.onInputValueChange);
     }
 
     enterInputValue(value: string) {
       this.input.value = value;
     }
 
-    private onInputValueChangeHandler = (value: string) => {
+    private onInputValueChange = (value: string) => {
       this.button.enabled = value.includes("@");
     };
   }
